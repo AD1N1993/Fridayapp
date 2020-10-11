7f74b9a19d83893d1044bf0804a1d7d5ee559c4b
YuryGrivickiy <griv1993@gmail.com>
2020-10-11 16:05:38 +0300

    Updates

---
+++ precache-manifest.ce8a52d735f1e6691fee1a10ba58546a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9950ee0edfa85bf2967398220d9f7f9e",
+    "url": "/Fridayapp/index.html"
+  },
+  {
+    "revision": "f2bb3ea746f79a1b2af3",
+    "url": "/Fridayapp/static/css/main.c6b4ecf1.chunk.css"
+  },
+  {
+    "revision": "70a7f3febaf37547f407",
+    "url": "/Fridayapp/static/js/2.76c98d11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Fridayapp/static/js/2.76c98d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2bb3ea746f79a1b2af3",
+    "url": "/Fridayapp/static/js/main.8f71d5f3.chunk.js"
+  },
+  {
+    "revision": "dbf2bed243d3509a5b46",
+    "url": "/Fridayapp/static/js/runtime-main.2465b693.js"
+  }
+]);
