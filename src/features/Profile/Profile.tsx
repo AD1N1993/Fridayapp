b0f9007573af412c3ac4551ea4ea81e553c44b4c
YuryGrivickiy <griv1993@gmail.com>
2020-10-19 13:55:49 +0300

    deleted .idea from Github

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {InputText} from "../../components/InputText/InputText";
 import {Button} from "../../components/Button/Button";
 
